111932c8a5126a36439e4282e190dc9c946911c5
kmsemo1 <32209919+kmsemo1@users.noreply.github.com>
2018-01-22 14:35:29 -0800

    added team.js

--- models/team.js
+++ models/team.js
@@ -1,7 +1,15 @@
 module.exports = function(sequelize, DataTypes) {
     var Team = sequelize.define("Team", {
       // Giving the Team model a name of type STRING
-      name: DataTypes.STRING
+      teamName: {
+          type: DataTypes.STRING
+      },
+      teamScore: {
+          type: DataTypes.INTEGER
+      },
+      userScore: {
+          type: DataTypes.INTEGER
+      }
     });
     return Team;
   };
