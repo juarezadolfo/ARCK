69964f1aeed67173d5ee3a2e0df50cf7b9c97a41
juarezadolfo <juarezadolfo@hotmail.com>
2018-01-25 11:54:04 -0800

    updating files to catch up

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,20 +1,36 @@
 var db = require("../models");
+var nflScore = require("../data/surveyScore")
 
 module.exports = function (app) {
 
     app.get("/", function (req, res) {
-        var query = {};
-        var nflScore = {};
-        for (var i=0; i<nflScore.length; i++) {
-            if (req.query.team_id===nflScore[i]) {
-                query.TeamId = req.query.team_id;
-            }
-            db.Team.findAll({}).then(function (dbTeam) {
-                res.json(dbTeam);
+        // var query = {};
+        // var nflScore = {};
+        // for (var i=0; i<nflScore.length; i++) {
+        //     if (req.query.team_id===nflScore[i]) {
+        //         query.TeamId = req.query.team_id;
+        //     }
+            db.NFLTeam.findAll({}).then(function (dbNFLTeam) {
+                res.json(dbNFLTeam);
             });
-        }
+        // }
       
     });
+
+
+    app.get("/nfl/:id", function(req, res) {
+        // Find one Author with the id in req.params.id and return them to the user with res.json
+       db.NFLTeam.findOne({
+         where: {
+            id: req.params.id
+         }
+       }).then(function(dbNFLTeam) {
+        // if the survey score is the id number call the findOne with that id number?
+        
+        res.json(dbNFLTeam);
+       });
+     });
+   
 
 
     // POST route for saving a new post
